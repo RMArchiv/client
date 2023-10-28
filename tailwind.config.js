693cea1915fae952d0160de1bc8688ee6f289494
Marcel Hering <rygos@icloud.com>
2023-10-29 01:21:32 +0200

    install livewire and tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+      './ressources/**/*.blade.php',
+      './ressources/**/*.js',
+      './ressources/**/*.vue',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
